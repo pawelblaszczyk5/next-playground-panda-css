90c917b59262034d43fcd1c9105520ed546aded5
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2023-06-19 23:31:11 +0200

    Move preset into the config file

--- panda.config.ts
+++ panda.config.ts
@@ -1,6 +1,42 @@
-import { defineConfig } from "@pandacss/dev";
+import { defineConfig, definePreset } from "@pandacss/dev";
+import basePreset from "@pandacss/preset-base";
 
-import { customPreset } from "~/theme/preset";
+export const customPreset = () => {
+	const { conditions, utilities } = basePreset;
+
+	return definePreset({
+		conditions: {
+			...conditions,
+			hoverAvailable: "@media (hover:hover) and (pointer:fine)",
+		},
+		theme: {
+			layerStyles: {
+				exampleLayerStyle: {
+					description: "example layer style",
+					value: {
+						border: "1px solid",
+						borderColor: "exampleColor",
+					},
+				},
+			},
+			tokens: {
+				colors: {
+					exampleColor: {
+						value: "#222",
+					},
+				},
+				fonts: {
+					body: {
+						value: "var(--font-inter), var(--font-fallback)",
+					},
+				},
+			},
+		},
+		utilities: {
+			...utilities,
+		},
+	});
+};
 
 export default defineConfig({
 	exclude: [],
